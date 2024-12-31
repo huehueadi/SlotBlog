66b761a7db428f3eb4e3ff936222831b5c321888
adty486 <aditya.ay519@gmail.com>
2024-12-31 22:28:24 +0530

    db url chnages

--- src/config/dbConfig.js
+++ src/config/dbConfig.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectionDatabase = async()=>{
     try {
-         await mongoose.connect("mongodb://localhost:27017/slotblog");
+         await mongoose.connect("mongodb+srv://temploginoffice:3H0ksQJqzYvUsS0U@cluster0.x4ynk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
          console.log("Database is connected")
     } catch (error) {
         console.log("Error while Connection to the Database", error)
