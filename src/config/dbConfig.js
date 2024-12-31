226e42e30ab5b2926ead3172c16696001272335a
adty486 <aditya.ay519@gmail.com>
2024-12-31 22:37:55 +0530

    db chnages

--- src/config/dbConfig.js
+++ src/config/dbConfig.js
@@ -1,13 +1,22 @@
 import mongoose from "mongoose";
 
-const connectionDatabase = async()=>{
-    try {
-         await mongoose.connect("mongodb+srv://temploginoffice:3H0ksQJqzYvUsS0U@cluster0.x4ynk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
-         console.log("Database is connected")
-    } catch (error) {
-        console.log("Error while Connection to the Database", error)
+const connectionDatabase = async () => {
+  try {
+    const mongoUri = "mongodb+srv://temploginoffice:3H0ksQJqzYvUsS0U@cluster0.x4ynk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+    if (!mongoUri) {
+      throw new Error("MongoDB URI is missing in environment variables.");
     }
-}
+    
+    await mongoose.connect(mongoUri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    
+    console.log("Database is connected");
+  } catch (error) {
+    console.error("Error while connecting to the Database", error);
+    process.exit(1); // Exit process if the DB connection fails
+  }
+};
 
-export default connectionDatabase
-
+export default connectionDatabase;
